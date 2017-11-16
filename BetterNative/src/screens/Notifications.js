918c4f8e8f9dc2501a5c80bf5171cd9b0b7fcceb
Matthew Hsu <gears88@gmail.com>
2017-11-16 16:51:27 -0500

    Add notifications view

--- BetterNative/src/screens/Notifications.js
+++ BetterNative/src/screens/Notifications.js
@@ -1,55 +1,77 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-import {Button, Container, Header, Content, Form, Item, Input, Left, Icon, Body, Right, Title} from "native-base";
+import {StyleSheet, Text, ListView} from 'react-native';
+import {Button, Container, Header, Content, Form, Item, Input, Left, Icon, Body, Right, Title, List, ListItem} from "native-base";
+import moment from "moment";
 
 
 export default class Notifications extends React.Component {
 
-    data = [{
-        date: new Date(),
+    static data = [{
+        date: moment("2017-11-16"),
         text: "You have authorized JPMorgan Chase and Co"
     }, {
-        date: new Date(),
+        date: moment("2017-11-16"),
         text: "New direct deposit setup by T-Mobile USA"
     }, {
-        date: new Date(),
+        date: moment("2017-11-15"),
         text: "You have a pending request from American Express"
     }, {
-        date: new Date(),
+        date: moment("2017-11-14"),
         text: "Verification of your passport is still pending"
     }, {
-        date: new Date(),
+        date: moment("2017-11-14"),
         text: "You have added a new passport to your account"
     }];
 
-    static navigationOptions = ({ navigation }) => ({
+    static navigationOptions = ({navigation}) => ({
         header: (
             <Header>
                 <Left>
                     <Button transparent onPress={() => navigation.navigate("DrawerOpen")}>
-                        <Icon name="menu" />
+                        <Icon name="menu"/>
                     </Button>
                 </Left>
                 <Body>
                 <Title>Notifications</Title>
                 </Body>
-                <Right />
+                <Right/>
             </Header>
         )
     });
 
-    const ds = new ListView.DataSource({
-        rowHasChanged: (r1, r2) => r1 !== r2,
-        sectionHeaderHasChanged: (s1, s2) => s1 !== s2
-    });
+    convertNotificationsArrayToMap(notifs) {
+        let dateCategoryMap = {}; // Create the blank map
+        notifs.forEach(function (notif) {
+            let dateBucket = notif.date.format("LL");
+            if (!dateCategoryMap[dateBucket]) {
+                // Create an entry in the map for the category if it hasn't yet been created
+                dateCategoryMap[dateBucket] = [];
+            }
+            dateCategoryMap[dateBucket].push(notif);
+        });
+
+        return dateCategoryMap;
+    }
+
 
     constructor(props) {
         super(props);
-        var CategoryMap = {};
-        if (!CategoryMap[divider]) {
-            CategoryMap[divider] = [];
-        }
-        CategoryMap[divider].push(row);
+
+        this.ds = new ListView.DataSource({
+            rowHasChanged: (r1, r2) => r1 !== r2,
+            sectionHeaderHasChanged: (s1, s2) => s1 !== s2
+        });
+    }
+
+    componentWillMount() {
+        //todo fetch notifications
+        let processedData = this.convertNotificationsArrayToMap(Notifications.data);
+
+        console.log(processedData);
+
+        this.setState({
+            dataSource: this.ds.cloneWithRowsAndSections(processedData)
+        });
     }
 
 
@@ -63,12 +85,12 @@ export default class Notifications extends React.Component {
                         </Body>
                     </Header>
                     <ListView
-                        dataSource={ds.cloneWithRowsAndSections(CategoryMap)}
+                        dataSource={this.state.dataSource}
                         renderRow={
-                            (detail) => < ListItem listItem ></ ListItem >
+                            (detail) => < ListItem><Text>{detail.text}</Text></ ListItem>
                         }
                         renderSectionHeader={
-                            (sectionData, category) => < ListItem itemDivider theme={baseTheme}>< Text></ Text></ ListItem>
+                            (sectionData, category) => < ListItem itemDivider><Text>{category}</ Text></ ListItem>
                         }
                     />
                 </Content>
