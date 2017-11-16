443d6a7f7b72fb9f3220122ae872e0e73a7dc5cb
Matthew Hsu <gears88@gmail.com>
2017-11-16 11:05:07 -0500

    Begin notifications list

--- BetterNative/src/screens/Notifications.js
+++ BetterNative/src/screens/Notifications.js
@@ -5,6 +5,23 @@ import {Button, Container, Header, Content, Form, Item, Input, Left, Icon, Body,
 
 export default class Notifications extends React.Component {
 
+    data = [{
+        date: new Date(),
+        text: "You have authorized JPMorgan Chase and Co"
+    }, {
+        date: new Date(),
+        text: "New direct deposit setup by T-Mobile USA"
+    }, {
+        date: new Date(),
+        text: "You have a pending request from American Express"
+    }, {
+        date: new Date(),
+        text: "Verification of your passport is still pending"
+    }, {
+        date: new Date(),
+        text: "You have added a new passport to your account"
+    }];
+
     static navigationOptions = ({ navigation }) => ({
         header: (
             <Header>
@@ -21,9 +38,20 @@ export default class Notifications extends React.Component {
         )
     });
 
+    const ds = new ListView.DataSource({
+        rowHasChanged: (r1, r2) => r1 !== r2,
+        sectionHeaderHasChanged: (s1, s2) => s1 !== s2
+    });
+
     constructor(props) {
         super(props);
+        var CategoryMap = {};
+        if (!CategoryMap[divider]) {
+            CategoryMap[divider] = [];
+        }
+        CategoryMap[divider].push(row);
     }
+
 
     render() {
         return (
@@ -34,10 +62,15 @@ export default class Notifications extends React.Component {
                         <Text>Alerts & Updates</Text>
                         </Body>
                     </Header>
-
-                    <Button block primary onPress={() => this.props.navigation.navigate('Profile', {name: 'Lucy'})}>
-                        <Text>Notification</Text>
-                    </Button>
+                    <ListView
+                        dataSource={ds.cloneWithRowsAndSections(CategoryMap)}
+                        renderRow={
+                            (detail) => < ListItem listItem ></ ListItem >
+                        }
+                        renderSectionHeader={
+                            (sectionData, category) => < ListItem itemDivider theme={baseTheme}>< Text></ Text></ ListItem>
+                        }
+                    />
                 </Content>
             </Container>
         )
