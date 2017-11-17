01bdc97507df95c5738942a155146cdc7d85a288
David % Frost <dfrost5@gmail.com>
2017-11-16 21:53:04 -0500

    add new identity dialog added

--- BetterNative/src/navigation/SidebarNavigator.js
+++ BetterNative/src/navigation/SidebarNavigator.js
@@ -6,6 +6,7 @@ import Authorizations from "../screens/Authorizations.js";
 import Wallet from "../screens/Wallet.js";
 import Profile from "../screens/Profile.js";
 import Settings from "../screens/Settings.js";
+import AddIdentity from "../screens/AddIdentity.js";
 
 import { DrawerNavigator, StackNavigator } from "react-navigation";
 
@@ -66,7 +67,8 @@ const WalletNavigator = StackNavigator({
 });
 
 const ProfileNavigator = StackNavigator({
-    Profile: {screen: Profile}
+    Profile: {screen: Profile},
+    AddIdentity: {screen: AddIdentity}
 });
 
 const SettingsNavigator = StackNavigator({
