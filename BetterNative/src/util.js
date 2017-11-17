e75c3fd58da9f52723852722fe6776f3317a2375
Matthew Hsu <gears88@gmail.com>
2017-11-16 21:35:26 -0500

    Add remote heroku endpoint to native

--- BetterNative/src/util.js
+++ BetterNative/src/util.js
@@ -1,3 +1,6 @@
 
 
-export const endpoint = "http://localhost:3000/api/";
+let remote = "https://better-service.herokuapp.com/api/";
+let local = "http://localhost:3000/api/";
+
+export const endpoint = remote;
